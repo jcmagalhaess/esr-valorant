83bc0235dce801be26c3e48b0b16cba0c80b8be3
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-05-16 09:16:55 -0300

    fix: adjusting of routes settings and native federation added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const APP_ROUTES: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: 'schedule', pathMatch: 'full'},
   { path: 'schedule', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },
   { path: 'sports', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },

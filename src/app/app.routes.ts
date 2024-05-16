8eb6b70c483cf258f104e1f036791ef0c7b5837d
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-05-15 23:00:27 -0300

    feat: init project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const APP_ROUTES: Routes = [
+  { path: '', redirectTo: 'schedule', pathMatch: 'full'},
+  { path: 'schedule', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },
+  { path: 'sports', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },
+  { path: 'live-play', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },
+  { path: 'statistics', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) },
+  { path: 'ranking', loadComponent: () => import('./test/test.component').then(c => c.TestComponent) }
+];
